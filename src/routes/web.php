a0067513e32a58ecf64bbe104ee22397b20d95f5
otsuka harune <rune_ririri@icloud.com>
2025-06-24 18:11:47 +0900

    完了

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[TodoController::class, 'index']);
+Route::get('/todos/search', [TodoController::class, 'search']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::patch('/todos/update', [TodoController::class, 'update']);
+Route::delete('/todos/delete', [TodoController::class, 'destroy']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::patch('/categories/{category_id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{category_id}', [CategoryController::class, 'destroy']);
